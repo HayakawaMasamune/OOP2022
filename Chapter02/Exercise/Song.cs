fe93d0870ca9efedd54de4a52dfedf0a5c83dda1
33199　早川匡宗 <33199@ojs.ac.jp>
2022-05-10 14:20:18 +0900

    第２章演習問題1

---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+        // プロパティ
+        public string Title { get; set; } 
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+    }
+}

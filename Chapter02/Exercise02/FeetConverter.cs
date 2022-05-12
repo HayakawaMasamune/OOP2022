03259e8a769dd96a15aec000a19f95fa1ff88b70
33199　早川匡宗 <33199@ojs.ac.jp>
2022-05-12 14:41:59 +0900

    第２章演習問題2.2

---
+++ Chapter02/Exercise02/FeetConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public class InchConverter {
+        //メートルからインチを求める
+        public double FromMeter(double meter)
+        {
+            return meter / 0.0254;
+        }
+        public double ToMeter(double inch )
+        {
+
+            return inch * 0.0254;
+        }
+    }
+}
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -8,6 +8,40 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args)
         {
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                PrintInchToMeterList(1, 10);
+            }
+            else
+            {
+                PrintMeterToInchList(1, 10);
+            }
         }
+        //フィートからメートルへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop)
+        {
+            InchConverter fc = new InchConverter();
+            for (int inch = 1; inch <= 10; inch++)
+            {
+                double meter = fc.ToMeter(inch);
+                Console.WriteLine("{0}inch = {1:0.0000} m", inch, meter);
+            }
+        }
+
+
+
+        private static void PrintInchToMeterList(int start, int stop)
+        {
+            InchConverter fc = new InchConverter();
+            for (int meter = 1; meter <= 10; meter++)
+            {
+                double inch = fc.ToMeter(meter);
+                Console.WriteLine("{0} ft = {1:0.0000} m", meter, inch);
+
+            }
+        }
+
+
     }
 }
+

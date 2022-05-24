97b02e3278e5e10caa421c6ca65b867b781b64c2
33199　早川匡宗 <33199@ojs.ac.jp>
2022-05-24 13:28:21 +0900

    問題4.2作成準備 問題4.1クラスライブラリ化

---
+++ Chapter04/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args)
+        {
+            //var ym = new YearMonth(2002, 12);
+
+           // var check = ym.Is21Century;
+           // var test = ym.AddOneMonth();
+
+           // Console.WriteLine(ym);
+        }
+    }
+}

00ee7be4ce27e2d618a9330e6be16346be4aa5af
33199　早川匡宗 <33199@ojs.ac.jp>
2022-05-31 14:06:45 +0900

    第5章　演習問題　

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args)
+        {
+            //5.1
+            var inputString1 = Console.ReadLine();
+            var inputString2 = Console.ReadLine();
+
+            if (String.Compare(inputString1, inputString2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+            Console.WriteLine("-------");
+
+        }
+    }
+}
---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            var line = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(line, out number)) {
+                Console.WriteLine("{0:#,#}", number);
+            } else {
+                Console.WriteLine("変換できませんでした");
+            }
+        }
+    }
+}

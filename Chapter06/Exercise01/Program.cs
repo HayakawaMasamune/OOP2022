b36570977693ef267db06625b3f46201d8ef589e
33199　早川匡宗 <33199@ojs.ac.jp>
2022-06-02 14:46:16 +0900

    第6章演習問題　6.1　完

--- Chapter06/Exercise01/Program.cs
+++ Chapter06/Exercise01/Program.cs
@@ -8,24 +8,24 @@ namespace Exercise01 {
     class Program {
         static void Main(string[] args)
         {
-            
-            
-                var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
 
-                Exercise1_1(numbers);
-                Console.WriteLine("-----");
 
-                Exercise1_2(numbers);
-                Console.WriteLine("-----");
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
 
-                Exercise1_3(numbers);
-                Console.WriteLine("-----");
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
 
-                Exercise1_4(numbers);
-                Console.WriteLine("-----");
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
 
-                Exercise1_5(numbers);
-            
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+
         }
 
         private static void Exercise1_1(int[] numbers)
@@ -39,7 +39,7 @@ namespace Exercise01 {
         {
             //最後から２つの要素の取り出し
 
-            foreach(var n in numbers.Skip(numbers.Length -2)) {
+            foreach (var n in numbers.Skip(numbers.Length - 2)) {
                 Console.WriteLine(n);
             }
         }
@@ -47,17 +47,27 @@ namespace Exercise01 {
         private static void Exercise1_3(int[] numbers)
         {
             //数値を文字列へ変換
+            var strs = numbers.Select(n => n.ToString());
+            foreach (var s in strs) {
+                Console.WriteLine(s);
+            }
 
         }
 
         private static void Exercise1_4(int[] numbers)
         {
             //小さい順に並べて先頭の3つを出力
-        }
+            foreach (var n in numbers.OrderBy(n => n).Take(3)) { 
+                Console.WriteLine(n);
 
-        private static void Exercise1_5(int[] numbers)
-        {
-            //重複を排除し10より大きい数がいくつあるか
+            }
         }
+            private static void Exercise1_5(int[] numbers)
+            {
+            //重複を排除し10より大きい数がいくつあるか
+            var count = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(count);
+            }
+        
     }
 }

a19ba6a0fbb41ab96c09e4be210ab5161ea256b4
33199　早川匡宗 <33199@ojs.ac.jp>
2022-06-14 14:29:56 +0900

    途中経過

--- Test202206/TestProject/Test01/Student.cs
+++ Test202206/TestProject/Test01/Student.cs
@@ -1,7 +1,17 @@
 ﻿namespace Test01 {
     class Student {
 
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
 
+        public Student(string Name,string Subject,int Score)
+        {
+            this.Name = Name;
+            this.Subject = Subject;
+            this.Score = Score;
+
+        }
 
     }
 }

96a8a04eba2878106cd0a06031087029ec30d4c1
33199　早川匡宗 <33199@ojs.ac.jp>
2022-07-05 14:26:15 +0900

    第１１章　演習問題

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -22,38 +22,66 @@ namespace Exercise1 {
             var newfile = "sports.xml";
             Exercise1_4(file, newfile);
 
-
+            //確認用
+            var text = File.ReadAllText(newfile);
+            Console.WriteLine(text);
         }
 
         private static void Exercise1_1(string file)
         {
-            var xdoc = XDocument.Load("sports.xml");
+            //匿名クラス　P284
+            var xdoc = XDocument.Load(file);
             var sports = xdoc.Root.Elements()
-                                .Select(x => new {
-                                    Name = x.Element("name").Value,
-                                    Teammembers = x.Element("teammembers").Value
-                                }) ;
+                             .Select(x => new {
+                                 Name = x.Element("name").Value,
+                                 Teammembers = x.Element("teammembers").Value
+                             });
             foreach (var sport in sports) {
-                Console.WriteLine("{0} ({1}",
-                                   sport.Name, sport.Teammembers);
+                Console.WriteLine("{0} {1}", sport.Name, sport.Teammembers);
             }
-
 
         }
 
         private static void Exercise1_2(string file)
         {
-            throw new NotImplementedException();
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                .Select(x => new {
+                    Firstplayed = x.Element("firsrplayed").Value,
+                    Name = x.Element("name").Attribute("kanji").Value
+                })
+                .OrderBy(x => int.Parse(x.Firstplayed));
+            foreach (var sport in sports) {
+                Console.WriteLine("{0}({1})", sport.Name, sport.Firstplayed);
+            }
         }
 
         private static void Exercise1_3(string file)
         {
-            throw new NotImplementedException();
+            var xdoc = XDocument.Load(file);
+            var sport = xdoc.Root.Elements()
+                .Select(x => new {
+                    Name = x.Element("name").Value,
+                    Teammembers = x.Element("teammembers").Value
+                })
+                .OrderByDescending(x => int.Parse(x.Teammembers)).FirstOrDefault();
+            Console.WriteLine("{0}({1})人)", sport.Name, sport.Teammembers);
         }
 
         private static void Exercise1_4(string file, string newfile)
         {
-            throw new NotImplementedException();
+            //要素の追加  P290
+            var element = new XElement("balldport",
+                new XElement("name", "サッカー"),
+                new XElement("kanji", "蹴球"),
+                new XElement("firstplayed", "1863"));
+
+            var xdoc = XDocument.Load("balldport");
+            xdoc.Root.Add(element);
+
+
+                
+
         }
     }
 }

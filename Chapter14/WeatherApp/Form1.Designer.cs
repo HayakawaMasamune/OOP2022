b11b09241fab18e1a10b10290f265295e18d7112
33199　早川匡宗 <33199@ojs.ac.jp>
2022-11-14 15:26:33 +0900

    11/14　途中

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDateTime = new System.Windows.Forms.TextBox();
+            this.cbPlace = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbpublishingOffice = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.Location = new System.Drawing.Point(151, 39);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(98, 35);
+            this.btWeatherGet.TabIndex = 0;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.Location = new System.Drawing.Point(151, 263);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.Size = new System.Drawing.Size(559, 113);
+            this.tbWeatherInfo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(60, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "天気概要";
+            // 
+            // tbDateTime
+            // 
+            this.tbDateTime.Location = new System.Drawing.Point(151, 188);
+            this.tbDateTime.Multiline = true;
+            this.tbDateTime.Name = "tbDateTime";
+            this.tbDateTime.Size = new System.Drawing.Size(216, 23);
+            this.tbDateTime.TabIndex = 3;
+            // 
+            // cbPlace
+            // 
+            this.cbPlace.FormattingEnabled = true;
+            this.cbPlace.Location = new System.Drawing.Point(151, 119);
+            this.cbPlace.Name = "cbPlace";
+            this.cbPlace.Size = new System.Drawing.Size(164, 20);
+            this.cbPlace.TabIndex = 4;
+            this.cbPlace.SelectedIndexChanged += new System.EventHandler(this.cbPlace_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(92, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "地域";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(60, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "報告日時";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(566, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "発表者";
+            // 
+            // tbpublishingOffice
+            // 
+            this.tbpublishingOffice.Location = new System.Drawing.Point(658, 39);
+            this.tbpublishingOffice.Name = "tbpublishingOffice";
+            this.tbpublishingOffice.Size = new System.Drawing.Size(100, 19);
+            this.tbpublishingOffice.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(485, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 19);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbpublishingOffice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbPlace);
+            this.Controls.Add(this.tbDateTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Controls.Add(this.btWeatherGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDateTime;
+        private System.Windows.Forms.ComboBox cbPlace;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbpublishingOffice;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //取得クリック
+        private void btWeatherGet_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+
+            var dString = wc.DownloadString($"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{textBox1.Text}.json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+
+            tbpublishingOffice.Text = json.publishingOffice;
+            tbWeatherInfo.Text = json.text;
+            tbDateTime.Text = json.reportDatetime.ToString();
+
+              
+        }
+
+        private void cbPlace_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = new ComboBox();
+
+            cb.Items.Add("北海道");
+            cb.Items.Add("東京");
+        }
+    }
+}//

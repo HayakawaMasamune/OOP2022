e60c12d5967cea4a61332ff0ef03fb6131644a6e
33199　早川匡宗 <33199@ojs.ac.jp>
2022-11-17 11:59:51 +0900

    WeatherApp 途中

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -36,14 +36,18 @@ namespace WeatherApp {
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.tbpublishingOffice = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbArea = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbArea = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btSelect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btWeatherGet
             // 
-            this.btWeatherGet.Location = new System.Drawing.Point(151, 39);
+            this.btWeatherGet.Location = new System.Drawing.Point(515, 104);
             this.btWeatherGet.Name = "btWeatherGet";
-            this.btWeatherGet.Size = new System.Drawing.Size(98, 35);
+            this.btWeatherGet.Size = new System.Drawing.Size(75, 31);
             this.btWeatherGet.TabIndex = 0;
             this.btWeatherGet.Text = "取得";
             this.btWeatherGet.UseVisualStyleBackColor = true;
@@ -69,7 +73,7 @@ namespace WeatherApp {
             // 
             // tbDateTime
             // 
-            this.tbDateTime.Location = new System.Drawing.Point(151, 188);
+            this.tbDateTime.Location = new System.Drawing.Point(151, 217);
             this.tbDateTime.Multiline = true;
             this.tbDateTime.Name = "tbDateTime";
             this.tbDateTime.Size = new System.Drawing.Size(216, 23);
@@ -78,7 +82,7 @@ namespace WeatherApp {
             // cbPlace
             // 
             this.cbPlace.FormattingEnabled = true;
-            this.cbPlace.Location = new System.Drawing.Point(151, 119);
+            this.cbPlace.Location = new System.Drawing.Point(331, 110);
             this.cbPlace.Name = "cbPlace";
             this.cbPlace.Size = new System.Drawing.Size(164, 20);
             this.cbPlace.TabIndex = 4;
@@ -96,7 +100,7 @@ namespace WeatherApp {
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(92, 118);
+            this.label3.Location = new System.Drawing.Point(285, 114);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 16);
             this.label3.TabIndex = 6;
@@ -106,7 +110,7 @@ namespace WeatherApp {
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(60, 188);
+            this.label4.Location = new System.Drawing.Point(60, 217);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 16);
             this.label4.TabIndex = 7;
@@ -116,7 +120,7 @@ namespace WeatherApp {
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(566, 39);
+            this.label5.Location = new System.Drawing.Point(439, 395);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 16);
             this.label5.TabIndex = 8;
@@ -124,24 +128,66 @@ namespace WeatherApp {
             // 
             // tbpublishingOffice
             // 
-            this.tbpublishingOffice.Location = new System.Drawing.Point(658, 39);
+            this.tbpublishingOffice.Location = new System.Drawing.Point(501, 392);
             this.tbpublishingOffice.Name = "tbpublishingOffice";
-            this.tbpublishingOffice.Size = new System.Drawing.Size(100, 19);
+            this.tbpublishingOffice.Size = new System.Drawing.Size(122, 19);
             this.tbpublishingOffice.TabIndex = 9;
             // 
-            // textBox1
+            // tbArea
             // 
-            this.textBox1.Location = new System.Drawing.Point(485, 148);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(137, 19);
-            this.textBox1.TabIndex = 10;
+            this.tbArea.Location = new System.Drawing.Point(230, 392);
+            this.tbArea.Name = "tbArea";
+            this.tbArea.Size = new System.Drawing.Size(137, 19);
+            this.tbArea.TabIndex = 10;
+            this.tbArea.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "エリアコード";
+            // 
+            // cbArea
+            // 
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Location = new System.Drawing.Point(331, 36);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(164, 20);
+            this.cbArea.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(285, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "地方";
+            // 
+            // btSelect
+            // 
+            this.btSelect.Location = new System.Drawing.Point(515, 28);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(75, 28);
+            this.btSelect.TabIndex = 14;
+            this.btSelect.Text = "確定";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btSelect);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbArea);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbArea);
             this.Controls.Add(this.tbpublishingOffice);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -154,6 +200,7 @@ namespace WeatherApp {
             this.Controls.Add(this.btWeatherGet);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,7 +218,11 @@ namespace WeatherApp {
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbpublishingOffice;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbArea;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbArea;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btSelect;
     }
 }
 
--- Chapter14/WeatherApp/Form1.cs
+++ Chapter14/WeatherApp/Form1.cs
@@ -23,7 +23,7 @@ namespace WeatherApp {
                 Encoding = Encoding.UTF8
             };
 
-            var dString = wc.DownloadString($"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{textBox1.Text}.json");
+            var dString = wc.DownloadString($"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{tbArea.Text}.json");
 
             var json = JsonConvert.DeserializeObject<Rootobject>(dString);
 
@@ -31,15 +31,228 @@ namespace WeatherApp {
             tbWeatherInfo.Text = json.text;
             tbDateTime.Text = json.reportDatetime.ToString();
 
-              
         }
 
         private void cbPlace_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ComboBox cb = new ComboBox();
+            if (cbArea.Text == "北海道地方") {
+                switch (cbPlace.SelectedIndex) {
+                    case 0:
+                        tbArea.Text = "011000";
+                        break;
+                    case 1:
+                        tbArea.Text = "012000";
+                        break;
+                    case 2:
+                        tbArea.Text = "013000";
+                        break;
+                    case 4:
+                        tbArea.Text = "014030";
+                        break;
+                    case 5:
+                        tbArea.Text = "014100";
+                        break;
+                    case 6:
+                        tbArea.Text = "015000";
+                        break;
+                    case 7:
+                        tbArea.Text = "016000";
+                        break;
+                    case 8:
+                        tbArea.Text = "01000";
+                        break;
+                }
 
-            cb.Items.Add("北海道");
-            cb.Items.Add("東京");
+            } else if (cbArea.Text == "東北地方") {
+                switch (cbPlace.SelectedIndex) {
+                    case 0:
+                        tbArea.Text = "020000";
+                        break;
+                    case 1:
+                        tbArea.Text = "030000";
+                        break;
+                    case 2:
+                        tbArea.Text = "040000";
+                        break;
+                    case 4:
+                        tbArea.Text = "050000";
+                        break;
+                    case 5:
+                        tbArea.Text = "060000";
+                        break;
+                    case 6:
+                        tbArea.Text = "070000";
+                        break;
+                }
+
+            } else if (cbArea.Text == "関東甲信地方") {
+                switch (cbPlace.SelectedIndex) {
+                    case 0:
+                        tbArea.Text = "080000";
+                        break;
+                    case 1:
+                        tbArea.Text = "090000";
+                        break;
+                    case 2:
+                        tbArea.Text = "100000";
+                        break;
+                    case 3:
+                        tbArea.Text = "110000";
+                        break;
+                    case 4:
+                        tbArea.Text = "120000";
+                        break;
+                    case 5:
+                        tbArea.Text = "130000";
+                        break;
+                    case 6:
+                        tbArea.Text = "140000";
+                        break;
+                    case 7:
+                        tbArea.Text = "190000";
+                        break;
+                    case 8:
+                        tbArea.Text = "200000";
+                        break;
+                }
+
+            } else if (cbArea.Text == "東海地方") {
+                switch (cbPlace.SelectedIndex) {
+                    case 0:
+                        tbArea.Text = "210000";
+                        break;
+                    case 1:
+                        tbArea.Text = "220000";
+                        break;
+                    case 2:
+                        tbArea.Text = "230000";
+                        break;
+                    case 3:
+                        tbArea.Text = "240000";
+                        break;
+                }
+
+            } else if (cbArea.Text == "北陸地方") {
+                switch (cbPlace.SelectedIndex) {
+                    case 0:
+                        tbArea.Text = "150000";
+                        break;
+                    case 1:
+                        tbArea.Text = "160000";
+                        break;
+                    case 2:
+                        tbArea.Text = "170000";
+                        break;
+                    case 3:
+                        tbArea.Text = "180000";
+                        break;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbArea.Items.Add("北海道地方");//0
+            cbArea.Items.Add("東北地方");//1
+            cbArea.Items.Add("関東甲信地方");//2
+            cbArea.Items.Add("東海地方");//3
+            cbArea.Items.Add("北陸地方");//4
+            cbArea.Items.Add("近畿地方");//5
+            cbArea.Items.Add("中国地方");//6
+            cbArea.Items.Add("四国地方");//7
+            cbArea.Items.Add("九州北部地方");//8
+            cbArea.Items.Add("九州南部・奄美地方");//9
+            cbArea.Items.Add("沖縄地方");//10
+    
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {        
+        }
+
+        private void btSelect_Click(object sender, EventArgs e)
+        {
+            cbPlace.Items.Clear();//初期化
+
+            switch (cbArea.SelectedIndex) {
+                case 0:
+                    cbPlace.Items.Add("宗谷地方");//0 
+                    cbPlace.Items.Add("上川・留萌地方");//1
+                    cbPlace.Items.Add("網走・北見・紋別地方");//2
+                    cbPlace.Items.Add("十勝地方");//3
+                    cbPlace.Items.Add("釧路・根室地方");//4
+                    cbPlace.Items.Add("胆振・日高地方");//5
+                    cbPlace.Items.Add("石狩・空知・後志地方");//6
+                    cbPlace.Items.Add("渡島・檜山地方");//7
+                    break;
+                case 1:
+                    cbPlace.Items.Add("青森県");//0
+                    cbPlace.Items.Add("岩手県");//1
+                    cbPlace.Items.Add("宮城県");//2
+                    cbPlace.Items.Add("秋田県");//3
+                    cbPlace.Items.Add("山形県");//4
+                    cbPlace.Items.Add("福島県");//5
+                    break;
+                case 2:
+                    cbPlace.Items.Add("茨城県");//0
+                    cbPlace.Items.Add("栃木県");//1
+                    cbPlace.Items.Add("群馬県");//2
+                    cbPlace.Items.Add("埼玉県");//3
+                    cbPlace.Items.Add("千葉県");//4
+                    cbPlace.Items.Add("東京都");//5
+                    cbPlace.Items.Add("神奈川県");//6
+                    cbPlace.Items.Add("山梨県");//7
+                    cbPlace.Items.Add("長野県");//8
+                    break;
+                case 3:
+                    cbPlace.Items.Add("岐阜県");//0
+                    cbPlace.Items.Add("静岡県");//1
+                    cbPlace.Items.Add("愛知県");//2
+                    cbPlace.Items.Add("三重県");//3
+                    break;
+                case 4:
+                    cbPlace.Items.Add("新潟県");//0
+                    cbPlace.Items.Add("富山県");//1
+                    cbPlace.Items.Add("石川県");//2
+                    cbPlace.Items.Add("福井県");//3
+                    break;
+                case 5:
+                    cbPlace.Items.Add("滋賀県");//0
+                    cbPlace.Items.Add("京都府");//1
+                    cbPlace.Items.Add("大阪府");//2
+                    cbPlace.Items.Add("兵庫県");//3
+                    cbPlace.Items.Add("奈良県");//4
+                    cbPlace.Items.Add("和歌山県");//5
+                    break;
+                case 6:
+                    cbPlace.Items.Add("鳥取県");//0
+                    cbPlace.Items.Add("島根県");//1
+                    cbPlace.Items.Add("岡山県");//2
+                    cbPlace.Items.Add("広島県");//3
+                    break;
+                case 7:
+                    cbPlace.Items.Add("徳島県");//0
+                    cbPlace.Items.Add("香川県");//1
+                    cbPlace.Items.Add("愛媛県");//2
+                    cbPlace.Items.Add("高知県");//3
+                    break;
+                case 8:
+                    cbPlace.Items.Add("山口県");//0
+                    cbPlace.Items.Add("福島県");//1
+                    cbPlace.Items.Add("佐賀県");//2
+                    cbPlace.Items.Add("長崎県");//3
+                    cbPlace.Items.Add("熊本県");//4
+                    cbPlace.Items.Add("大分県");//5
+                    cbPlace.Items.Add("宮崎県");//6
+                    cbPlace.Items.Add("鹿児島県");//7
+                    break;
+                case 9:
+                    cbPlace.Items.Add("沖縄本島地方");//0
+                    cbPlace.Items.Add("大東島地方");//1
+                    cbPlace.Items.Add("宮古島地方");//2
+                    cbPlace.Items.Add("八重山地方");//3
+                    break;
+            }
         }
     }
-}//
+}
+       
